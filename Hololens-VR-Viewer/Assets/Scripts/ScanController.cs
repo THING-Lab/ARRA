09ea75c134e4604d4161b7b8dac17140e345589e
Chandan Naik <chandan94nagarajnaik@gmail.com>
2019-10-14 18:28:54 -0600

    add oculus controller models

--- Hololens-VR-Viewer/Assets/Scripts/ScanController.cs
+++ Hololens-VR-Viewer/Assets/Scripts/ScanController.cs
@@ -20,12 +20,12 @@ public class ScanController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float triggerVal = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        float triggerVal = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.LTouch);
         if (triggerVal > 0.4f && !wasTriggerPress) {
             environments[currentEnv].SetActive(false);
 
             currentEnv += 1;
-            if (currentEnv > environments.Length) currentEnv = 0;
+            if (currentEnv >= environments.Length) currentEnv = 0;
 
             environments[currentEnv].SetActive(true);
         }
