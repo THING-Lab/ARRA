a7dc4a107f9cbd9b36885cc5e2ede95fff5768e0
petroochio <ptgyory@gmail.com>
2020-04-12 18:36:48 -0600

    temp

---
+++ Hololens-Novice-App/Assets/Scripts/ScanIndicator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanIndicator : MonoBehaviour
+{
+    public GameObject red;
+    public GameObject green;
+    public GameObject white;
+    // Start is called before the first frame update
+    void Start()
+    {
+        red.SetActive(true);
+        green.SetActive(false);
+        white.SetActive(false);
+    }
+
+    public void SetCanScan() {
+        red.SetActive(false);
+        green.SetActive(true);
+        white.SetActive(false);
+    }
+
+    public void SetCannotScan() {
+        red.SetActive(true);
+        green.SetActive(false);
+        white.SetActive(false);
+    }
+
+    public void SetIsScanning() {
+        red.SetActive(false);
+        green.SetActive(false);
+        white.SetActive(true);
+    }
+}
